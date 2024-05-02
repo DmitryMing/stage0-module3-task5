13d22521807ac0c43d286db995331ed42524efc8
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-05-02 19:25:29 +0800

    go

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        double res = minutes * 60.0;
+        System.out.println(res);
     }
 }
